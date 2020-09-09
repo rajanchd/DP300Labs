45290c94f4efea957e7eb47e472a95e0f18044e7
KALEN DELANEY <30476810+kalendelaney@users.noreply.github.com>
2020-09-08 19:51:21 -0700

    Update DP300-Lab-Module-6.ps1

--- Allfiles/Labs/Automate Resources/DP300-Lab-Module-6.ps1
+++ Allfiles/Labs/Automate Resources/DP300-Lab-Module-6.ps1
@@ -3,6 +3,6 @@ $AzureDatabase = "sample-db-with-tde"
 $Cred = Get-AutomationPSCredential -Name "SQLUser"
 $SQLOutput = $(Invoke-Sqlcmd -ServerInstance $AzureSQLServerName `
     -Username $Cred.Username -Password $Cred.GetNetworkCredential().Password `
-    -Database $AzureDatabase -Query "EXEC dbo.usp_AdaptiveIndexDefrag" -Verbose) 4>&1
+    -Database $AzureDatabase -Query "EXEC dbo.usp_AdaptiveIndexDefrag" -Verbose) 
 
 Write-Output $SQLOutput
