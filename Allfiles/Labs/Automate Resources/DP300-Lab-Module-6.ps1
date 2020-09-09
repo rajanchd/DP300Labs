e17ecfadde0bc22874d5f7f45d52b41a396d86fa
KALEN DELANEY <30476810+kalendelaney@users.noreply.github.com>
2020-09-08 19:49:07 -0700

    Update DP300-Lab-Module-6.ps1
    Removed space in between - and Name on line 3.

--- Allfiles/Labs/Automate Resources/DP300-Lab-Module-6.ps1
+++ Allfiles/Labs/Automate Resources/DP300-Lab-Module-6.ps1
@@ -1,6 +1,6 @@
 $AzureSQLServerName = " "
 $AzureDatabase = "sample-db-with-tde"
-$Cred = Get-AutomationPSCredential - Name "SQLUser"
+$Cred = Get-AutomationPSCredential -Name "SQLUser"
 $SQLOutput = $(Invoke-Sqlcmd -ServerInstance $AzureSQLServerName `
     -Username $Cred.Username -Password $Cred.GetNetworkCredential().Password `
     -Database $AzureDatabase -Query "EXEC dbo.usp_AdaptiveIndexDefrag" -Verbose) 4>&1
